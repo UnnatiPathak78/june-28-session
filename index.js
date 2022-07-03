dfba39f0466ac1003b3432aa890469c20618175f
Unnati Pathak <unnatipathak78@gmail.com>
2022-07-03 07:13:50 +0530

    June-28-session

---
+++ index.js
@@ -0,0 +1,69 @@
+/*/console.log("1" + "2");
+console.log(1 + "2");
+console.log(1 + 1 + "2");
+console.log(1 + 1 + "1" + 1 + 1);
+console.log(2 + 3 + "4" - 10 + 5);
+console.log(123 % 6);
+console.log(123 / 6);*/
+
+/*let num = 23;
+if(num % 2 == 0){
+    console.log("Number is even");
+}
+else{
+    console.log("number is odd");
+}*/
+
+
+/*let num=5;
+if(num % 3 == 0 && num % 5 == 0){
+    console.log("FIZZBUZZ");
+}
+else if(num % 3== 0){
+    console.log("FIZZ");
+}
+else if(num % 5 == 0){
+    console.log("BUZZ");
+}
+else{
+    console.log(num);
+}*/
+
+
+let num=1;
+sum = 1;
+while(num<10){
+    console.log(sum);
+    num=num+1;
+    sum = num + sum;
+}
+
+
+
+/*let num=20;
+while(num>0){
+    console.log(num);
+    num=num-1;
+}*/
+
+
+/*let num = 1;
+while(num <= 100){
+    
+    if(num % 2 == 0){
+        console.log(num);
+    }
+    num = num + 1;
+   
+}*/
+
+
+
+/*let num = 20;
+let i = 1 ;
+while(num <= 20 && num < i){
+    if (num % i == 0){
+        console.log("number is prime number "+ num);
+    }
+    num = i + num;
+}*/
